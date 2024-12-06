2ebf0ba8a6e376e9c22620881a1b3e6565dc21aa
= <=>
2024-12-06 14:45:52 +0900

    MOdify Pol ..

--- ctrl.py
+++ ctrl.py
@@ -9,7 +9,20 @@ class Control:
     def connectSignals(self):
         self.view.btn1.clicked.connect(self.calculate)
         self.view.btn2.clicked.connect(self.view.clearMessage)
-        
+
+    def mul(self, a, b):
+        try:
+            return a*b
+        except:
+            return "Calculation Error"
+
+
+    def pol(self, a, b):
+        try:
+            return a*b
+        except:
+            return "Calculation pol Error"
+
     def sum(self, a, b):
         try:
             return a+b
